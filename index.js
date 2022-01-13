f11189591f24e72566bf66148c447d94338c8b8e
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-13 09:05:09 -0500

    working

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+//function1
+const returnFirstTwoDrivers = function (drivers) {
+    console.log('first two drivers');
+    return drivers.slice(0, 2)
+}
+//function2
+const returnLastTwoDrivers = function (drivers) {
+    console.log('last two drivers');
+    return drivers.slice(2, 4)
+}
+
+//function3
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+console.log('selecting drivers');
+
+
+//function4- not passing?
+function creatFareMultiplier(fare, number) {
+    console.log('fare charge')
+    return (fare, number) => (fare * number);
+}
+
+//function5
+const fareDouble = createFareMultiplier(2);
+
+//function6
+const fareTriple = function() {
+    return createFareMultiplier(fare, 3)
+}
+
+//function 7
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    return [returnFirstTwoDrivers, returnLastTwoDrivers];
+}
